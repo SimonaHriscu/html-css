46e8d938997dfcbc851d00d626ac4c13554e4bb2
Simona <see.moona@yahoo.com>
2020-04-29 13:29:31 +0200

    basic calculator in js

--- Exercises Class/29.04.20 Calculator/index.js
+++ Exercises Class/29.04.20 Calculator/index.js
@@ -1,26 +1,11 @@
-
-function inputOne(){
-    let valOne = document.querySelector("#firstNumber").value;
-    if (valOne == "") {
-     return alert("You cannot have an empty input");
-        }
-     }
-
- function inputTwo(){
-    let valTwo = document.querySelector("#secondNumber").value;
-    if (valTwo == "") {
-        return alert("You cannot have an empty input");
-    }
- }
-
 function calc(){
-    let result = document.querySelector("#sumResult");
     let elemOne = document.querySelector("#firstNumber").value
     let number1 = parseInt(elemOne);
     
     let elemTwo = document.querySelector("#secondNumber").value;
     let number2 = parseInt(elemTwo);
     
+    let result = document.querySelector("#sumResult");
     let sum = number1 + number2;
     result.value = sum;
     return sum;
